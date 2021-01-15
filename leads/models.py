81ea4e34cfdc67c2afd830bd1f8d88e4cc2c0680
Soubhagya <nikusoubhagyaranjan@gmail.com>
2021-01-15 11:48:57 +0530

    created Agent model

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+class Agent(models.Model):
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
 
 class Lead(models.Model):
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
