d9cb057cc89ab590e6edbfbf101b2cafda6b08fe
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-01-27 20:09:14 +0530

    save input lead form

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
 from .forms import LeadForm
 
 def lead_list(request):
@@ -17,8 +17,26 @@ def lead_detail(request, pk):
     return render(request, 'leads/lead_detail.html', context)  
 
 def lead_create(request):
-    print(request.POST)
+    form = LeadForm()
+    if request.method == 'POST':
+        print('Receiving a post request...')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print('The form is valid')
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            print('A new lead has been created')
+            return redirect('/leads')
     context = {
-        'form': LeadForm()
+        'form': form
     }
     return render(request, "leads/lead_create.html", context)
